e2a68415411b9e8154767f02a9c447d3f172ba09
JoseA4718 <49050880+JoseA4718@users.noreply.github.com>
2021-03-24 12:30:51 -0600

    unit testing

---
+++ src/com/company/CalculadoraTest.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import static org.junit.Assert.*;
+
+public class CalculadoraTest {
+
+    @org.junit.Test
+    public void operar() {
+        assertEquals(5,2+2);
+    }
+}
